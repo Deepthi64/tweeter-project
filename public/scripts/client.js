aadf090bd84a81cdc6f08ece2f11a38db49ba739
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-29 00:59:51 -0400

    final submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,86 +6,90 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function() {
+
+  const loadTweets = function() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json",
+      success: function(tweets) {
+        renderTweets(tweets);
+      },
+      error: function(error) {
+        console.error(error);
+      }
+    });
+  };
+  
   $('#new-tweet-form').on('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission behaviour
+    event.preventDefault();
+
+    const $error = $(this).find('.error-msg');
+    $error.hide(); 
     // Code to handle the form submission using AJAX
+    const tweetText = $(this).find('textarea').val().trim(); 
+    const maxLength = 140;
     
-    const tweetData = $(this).serialize(); // Serialize the form data into a query string
-   
+    // Check if the tweet is empty or exceeds the maximum length
+    if (!tweetText) {
+      $error.text('Error: Your tweet cannot be empty').slideDown();
+    } else if (tweetText.length > maxLength) {
+      $error.text('Error: Your tweet is too long').slideDown(); 
+    } else {
+      const tweetData = $(this).serialize(); 
     $.ajax({
       method: 'POST',
       url: '/tweets',
       data: tweetData
     })
     .then(function(response) {
-      console.log('Tweet submitted:', response);
-      // Code to update the UI with the new tweet
+      $(this).find('textarea').val(''); 
+      $(this).find('.counter').text(maxLength); 
+       loadTweets();
+       $error.hide();
     })
     .catch(function(error) {
       console.error('Error submitting tweet:', error);
     });
-  });
-});
- 
-
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
   }
-]
+  loadTweets();
+});
+
+
+
 
 const createTweetElement = function(tweet) {
   let $tweet = $("<article>").addClass("tweet-container");
-  
-  let $header = $("<header>");
-  let $avatar = $("<img>").addClass("avatar").attr("src", tweet.user.avatars);
+  let $header = $("<span>").addClass("tweetHeader");
+  let $avatar = $("<img/>").addClass("avatar").attr("src", tweet.user.avatars);
   let $name = $("<h2>").text(tweet.user.name);
-  let $handle = $("<p>").addClass("handle").text(tweet.user.handle);
-  
-  $header.append($avatar).append($name).append($handle);
-  
+  let $handle = $("<p1>").addClass("handle").text(tweet.user.handle);
+  let $span = $("<span>").addClass("avatarNameContainer");
+  $span.append($avatar).append($name);
+  $header.append($span).append($handle);
   let $content = $("<div>").addClass("userTweet");
   let $text = $("<p>").text(tweet.content.text);
-  
   $content.append($text);
-  
   let $footer = $("<footer>").addClass("footer");
   let $timeAgo = $("<span>").addClass("timestamp").text(moment(tweet.created_at).fromNow());
+  let $iconSpan = $("<span>");
   let $heart = $("<i>").addClass("fas fa-heart");
   let $retweet = $("<i>").addClass("fas fa-retweet");
   let $flag = $("<i>").addClass("fas fa-flag");
-  
-  $footer.append($timeAgo).append($heart).append($retweet).append($flag);
-  
-  $tweet.append($header).append($content).append($footer);
-  
-  return $tweet;
+  $iconSpan.append($heart).append($retweet).append($flag);
+  $footer.append($timeAgo).append($iconSpan);
+   $tweet.append($header).append($content).append($footer);
+   return $tweet;
 }
 
 const renderTweets = function(tweets) {
-  for (let tweet of tweets) {
-    let $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet);
-  }
+  const $tweetsContainer = $('#tweets-container');
+  $tweetsContainer.empty(); 
+
+  tweets.forEach(function(tweet) {
+    const $tweet = createTweetElement(tweet);
+    $tweetsContainer.prepend($tweet);
+  });
 }
 
-renderTweets(tweetData);
+});
